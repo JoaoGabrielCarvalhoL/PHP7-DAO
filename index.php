61df22b5b720d2a706c99666f06ffd84729b5a6b
João Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-09-29 12:53:56 -0300

    Class Usuario

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
 require_once("config.php"); 
 
 
-$sql = new Sql(); 
+$root = new Usuario(); 
+$root->loadById(2); 
 
-$usuarios = $sql->select("SELECT FROM * tb_usuarios"); 
-
-echo json_encode($usuarios);
-
+echo $root;
 
 
 
