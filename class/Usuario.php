0ee03d514a1cf344707e014736b76fa384218b98
João Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-09-30 10:22:06 -0300

    Delete

--- class/Usuario.php
+++ class/Usuario.php
@@ -136,6 +136,20 @@ class Usuario
 		$sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(':LOGIN'=>getDeslogin(), ':PASSWORD'=>getDessenha(), ':ID'=>getIdusuario()));
 	}
 
+
+	public function delete()
+	{
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(':ID'=>$this->getIdusuario()));
+
+		$this->setIdusuario(0); 
+		$this->setDeslogin("");
+		$this->setDessenha("");
+		$this->setDtcadastro(new DateTime()); 
+
+	}
+
 	public function __construct($login="", $password="")
 	{
 		$this->setDeslogin($login); 
